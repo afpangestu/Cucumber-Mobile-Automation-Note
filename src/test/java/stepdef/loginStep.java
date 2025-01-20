a4536fd5dfc0bd845efe4613b99eeba77104e350
Aji Fauzi P <ajifauzi949@gmail.com>
2025-01-20 18:19:35 +0700

    add loginStep.java

---
+++ src/test/java/stepdef/loginStep.java
@@ -0,0 +1,26 @@
+package stepdef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginStep {
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+
+    }
+
+    @When("the user enters username and password")
+    public void theUserEntersUsernameAndPassword() {
+    }
+
+    @And("the user click login button")
+    public void theUserClickLoginButton() {
+    }
+
+    @Then("the user is redirected to home activity")
+    public void userSuccessfullyLogin(){
+
+    }
+}
