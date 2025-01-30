fd11531f817051b07b58e0478d6910b9c4a94d40
Aji Fauzi P <ajifauzi949@gmail.com>
2025-01-31 06:29:17 +0700

    update junit > testng

--- src/test/java/stepdef/loginStep.java
+++ src/test/java/stepdef/loginStep.java
@@ -8,7 +8,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.Assert;
+import org.testng.Assert;
 
 import java.net.MalformedURLException;
 import java.net.URI;
