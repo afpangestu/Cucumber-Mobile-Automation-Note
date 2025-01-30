10ed2a9624a38da8a69b79f91ce4bf43aac92991
Aji Fauzi P <ajifauzi949@gmail.com>
2025-01-30 15:14:17 +0700

    update locator and methods

--- src/main/java/activity/LoginActivity.java
+++ src/main/java/activity/LoginActivity.java
@@ -2,24 +2,30 @@ package activity;
 
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LoginActivity {
     AppiumDriver driver;
+    WebDriverWait wait;
 
     // contructor
     public LoginActivity(AppiumDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     // locator
     By loginTitleTxt = By.xpath("//android.widget.TextView[@text='Login']");
     By registerBtn = By.id("com.kazakimaru.ch04_ajifauzipangestu:id/txt_daftar");
     By loginBtn = By.id("com.kazakimaru.ch04_ajifauzipangestu:id/btn_login");
-    By usernameEt = By.xpath("//android.widget.EditText[@text='Username']");
-    By passwordEt = By.xpath("//android.widget.EditText[@text='Password']");
+    By usernameEt = By.xpath("(//android.widget.EditText[contains(text(),'')])[1]");
+    By passwordEt = By.xpath("(//android.widget.EditText[contains(text(),'')])[2]");
     By usernameEmptyMsg = By.xpath("//android.widget.TextView[@resource-id='com.kazakimaru.ch04_ajifauzipangestu:id/textinput_error' and @text='Username tidak boleh kosong!']");
     By passwordEmptyMsg = By.xpath("//android.widget.TextView[@resource-id='com.kazakimaru.ch04_ajifauzipangestu:id/textinput_error' and @text='Password tidak boleh kosong!']");
-    By toastMsg = By.xpath("//android.widget.Toast[@text='Username atau Password salah']");
+    By toastMsg = By.xpath("(//android.widget.Toast[1]");
 
 
     // methods
@@ -50,7 +56,7 @@ public class LoginActivity {
     }
 
     public boolean getToastMsg() {
-        return driver.findElement(toastMsg).isDisplayed();
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(toastMsg)).isDisplayed();
     }
 
     public boolean loginTitle() {
