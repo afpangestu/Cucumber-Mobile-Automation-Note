e0ea2634cbb2981945c1d71239d98c7c9b0bfef9
Aji Fauzi P <ajifauzi949@gmail.com>
2025-01-31 09:21:27 +0700

    add single custom runner for all tests

--- src/test/java/runner/customRunner.java
+++ src/test/java/runner/customRunner.java
@@ -1,5 +1,6 @@
 package runner;
 
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.Test;
 
@@ -10,4 +11,4 @@ import org.testng.annotations.Test;
         plugin = {"io.qase.cucumber7.QaseEventListener", "pretty", "html:target/HTML_report.html", "json:target/cucumber_report.json"}
 )
 
-public class customRunner { }
+public class customRunner extends AbstractTestNGCucumberTests { }
